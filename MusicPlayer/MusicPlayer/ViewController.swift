3eaed74a85ff532fb8b8787665ae8bdffbc3b14b
geeunii <koo4934@naver.com>
2024-07-31 22:12:59 +0900

    뮤직 플레이어

--- MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -6,13 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    // MARK: - Properties
+    var player: AVAudioPlayer!
+    var timer: Timer!
     
     // MARK: IBOutlets
     @IBOutlet var playPauseButton: UIButton!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
+    
+    // MARK: - Methods
+    // MARK: Custom Method
+    func initializePlayer() {
+        
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            print("음원 파일 에셋을 가져올 수 없습니다.")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메시지 : \(error.localizedDescription)")
+        }
+        
+        
+    }
     
     
     
@@ -21,6 +46,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        
+        print("button tapped")
+        
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
 }
 
